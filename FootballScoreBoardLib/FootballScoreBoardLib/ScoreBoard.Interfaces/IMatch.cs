68c883c176c851b0da04eddcebdac8adda8b6d4d
shree-bhimanavaru <shree-bhimanavaru@users.noreply.github.com>
2025-03-22 09:48:24 +0100

    Initial-Commit: Adding interfaces for Scoreboard and Match

---
+++ FootballScoreBoardLib/FootballScoreBoardLib/ScoreBoard.Interfaces/IMatch.cs
@@ -0,0 +1,13 @@
+﻿namespace FootballScoreBoardLib.Interfaces
+{
+    public interface IMatch
+    {
+        string HomeTeam { get; }
+        string AwayTeam { get; }
+        int HomeScore { get; }
+        int AwayScore { get; }
+        bool IsInProgress { get; }
+        void UpdateScore(int homeScore, int awayScore);
+        void FinishMatch();
+    }
+}
---
+++ FootballScoreBoardLib/FootballScoreBoardLib/ScoreBoard.Interfaces/IScoreboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using FootballScoreBoardLib.Interfaces;
+
+namespace ScoreboardLibrary.Interfaces
+{
+    public interface IScoreboard
+    {
+        void StartMatch(string homeTeam, string awayTeam);
+        void UpdateScore(string homeTeam, string awayTeam, int homeScore, int awayScore);
+        void FinishMatch(string homeTeam, string awayTeam);
+        IEnumerable<IMatch> GetMatchesInProgressOrderedByScore();
+    }
+}
